bb9c96b78912175a0259dc8c2cd4d6618abfafa9
ДМ. Па <qtckpuhdsa@gmail.com>
2020-06-23 23:08:34 +0300

    -

--- app.py
+++ app.py
@@ -75,4 +75,4 @@ def webhook():
 
 
 if __name__ == "__main__":
-    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 2000)))
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 33500)))
